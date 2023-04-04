6516e2a6964cb64b018c13c2b8b54a6f990a3664
OlteanuCatalin <olteanuionutcatalin02@gmail.com>
2023-04-04 18:20:09 +0300

    Update: Added new activity

--- app/src/main/java/com/example/lab2_ppa/MainActivity.java
+++ app/src/main/java/com/example/lab2_ppa/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.lab2_ppa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.lab2_ppa.user.User;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button button;
 
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email=findViewById(R.id.Mail_EditText);
+        password=findViewById(R.id.Passwrod_EditText);
+        button=findViewById(R.id.Sign_Up_Button);
+
+        user=new User();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user.setMail(email.getText().toString());
+                user.setPassword(password.getText().toString());
+
+                Intent intent=new Intent(getApplicationContext(),Profile.class);
+                intent.putExtra("email",user.getMail());
+                intent.putExtra("password",user.getPassword());
+
+                startActivity(intent);
+            }
+        });
     }
 }
